e5bcc3fe93cdadcf81762b1d13dbc225d48e63aa
Hamdhan Basheer <hbasheer@lab3r8s5.42abudhabi.ae>
2024-10-17 15:00:40 +0400

    token count works with operands

--- tokenize.c
+++ tokenize.c
@@ -1,48 +1,20 @@
 #include "minishell.h"
 
-int ft_quote(char *str, int i, char quote)
+int	ft_quote(char *str, int i, char quote)
 {
 	if (quote == '\'')
 	{
-		while(str[i] && str[i] != '\'')
+		while (str[i] && str[i] != '\'')
 			i++;
-		// i++;
-		return(i);
+		return (i);
 	}
 	else
 	{
-		while(str[i] && str[i] != '\"')
+		while (str[i] && str[i] != '\"')
 			i++;
-		// i++;
-		return(i);
+		return (i);
 	}
 }
-
-// int	ft_wount(char *s)
-// {
-// 	int	i;
-// 	int	word;
-
-// 	i = 0;
-// 	word = 0;
-// 	while (s[i])
-// 	{
-// 		while (s[i] && (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13)))
-// 			i++;
-// 		if (s[i] && s[i] != ' ' && !(s[i] >= 9 && s[i] <= 13))
-//         {
-//             if(s[i] == '\'' || s[i] == '\"')
-// 			{
-// 				i++;
-//                 i = ft_quote(s, i, s[i - 1]);
-//             }
-//             word++;
-//         }
-// 		while (s[i] && s[i] != ' ' && !(s[i] >= 9 && s[i] <= 13))
-// 			i++;
-// 	}
-// 	return (word);
-// }
 
 int	ft_wount(char *s)
 {
@@ -55,30 +27,39 @@ int	ft_wount(char *s)
 	{
 		while (s[i] && (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13)))
 			i++;
-		if (s[i] && s[i] != ' ' && !(s[i] >= 9 && s[i] <= 13))
+		if (s[i] && s[i] != ' ' && !(s[i] >= 9 && s[i] <= 13) && !(s[i] == '<'
+				|| s[i] == '>' || s[i] == '|'))
 			word++;
 		while (s[i] && s[i] != ' ' && !(s[i] >= 9 && s[i] <= 13))
 		{
-			if((s[i] == '\'' || s[i] == '\"') && (s[i - 1] = ' ' || (s[i - 1] >= 9 && s[i - 1] <= 13)))
-				i = ft_quote(s, i+1, s[i]);
-			else if((s[i] == '\'' || s[i] == '\"') && s[i - 1] != ' ' && !(s[i - 1] >= 9 && s[i - 1] <= 13))
+			if ((s[i] == '<' || s[i] == '>' || s[i] == '|'))
+			{
+				word++;
+				i++;
+				break ;
+			}
+			if ((s[i] == '\'' || s[i] == '\"') && (s[i - 1] = ' ' || (s[i
+					- 1] >= 9 && s[i - 1] <= 13)))
+				i = ft_quote(s, i + 1, s[i]);
+			else if ((s[i] == '\'' || s[i] == '\"') && s[i - 1] != ' ' && !(s[i
+					- 1] >= 9 && s[i - 1] <= 13))
 				i = ft_quote(s, i + 1, s[i]);
 			i++;
-		}	
+		}
 	}
-	return(word);
+	return (word);
 }
 
-
-char **ft_tokenize(char *s)
+char	**ft_tokenize(char *s)
 {
-    char *r;
-    char **split;
+	char *r;
+	char **split;
 
-    split = NULL;
-    r = ft_strtrim(s, " ");
+	split = NULL;
+	r = ft_strtrim(s, " ");
 	printf("\nafter_trim - {%s}\n", r);
-    int i = ft_wount(r);
-    printf("\ntoken_count - %d\n", i);
-    return(split);
+	int i = ft_wount(r);
+	printf("\ntoken_count - %d\n", i);
+	free(r);
+	return (split);
 }
