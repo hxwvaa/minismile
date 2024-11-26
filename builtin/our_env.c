89f2ece1bc46b41f80c22afdf7cdc7b2bc1fbde2
Hamdhan Basheer <hbasheer@lab1r2s1.42abudhabi.ae>
2024-11-26 17:36:48 +0400

    export gets sorted (im not freeing it yet bc it doesnt free it properly so i will see what im doing wrong) but export is not working properly, for example if i type (export dasdasdas) it gets added and then if i export something again like (dasda=dasda) the previous one disappears 💀 it doesnt happen everytime tho

--- builtin/our_env.c
+++ builtin/our_env.c
@@ -26,12 +26,56 @@ void our_env(t_list *envir)
     }
 }
 
+t_list *clone_list(t_list *o_list)
+{
+    t_list *list;
+    t_list *temp;
+    t_list *new_l;
+
+    list = o_list;
+    new_l = NULL;
+    while(list)
+    {
+        temp = ft_lstnew(list->content);
+        if (!temp)
+            return(NULL);
+        ft_lstadd_back(&new_l, temp);
+        list = list->next;
+    }
+    return(new_l);
+}
+
+t_list *sort_exp(t_list *list)
+{
+    t_list *final;
+    t_list *n_list;
+    void *temp;
+
+    n_list = clone_list(list);
+    if(!n_list)
+        return(NULL);
+    final = n_list;
+    while(n_list)
+    {
+        if (n_list->next && ft_strncmp((char *)n_list->content, (char *)n_list->next->content, ft_strlen((char *)n_list->content)) > 0)
+        {
+            temp = n_list->content;
+            n_list->content = n_list->next->content;
+            n_list->next->content = temp;
+            n_list = final;
+        }
+        else
+            n_list = n_list->next;
+    }
+    return(final);
+}
+
 void our_expenv(t_shell *data)
 {
     t_list *tmp;
-
-    tmp = data->envir;
-
+    tmp = sort_exp(data->envir);
+    // if (!tmp)
+    //     ; //ERROR
     while(tmp)
     {
         char *equal = ft_strchr((char *)tmp->content, '=');
